135aa69f1d6e918b0b2617fc34e81a4b4648984b
leonelgomez1990 <leonelgomez@live.com.ar>
2021-09-01 22:53:43 -0300

    App inicial de prueba con: boton y listener- constraint layout

--- 01/Test/app/src/main/java/com/lfg/test/MainActivity.kt
+++ 01/Test/app/src/main/java/com/lfg/test/MainActivity.kt
@@ -2,10 +2,27 @@ package com.lfg.test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var txtCartel : TextView
+    lateinit var btnShow : Button
+    var textoCartel : String = "Hola Leo"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        txtCartel = findViewById(R.id.txtShow)
+        btnShow = findViewById(R.id.btnShow)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        btnShow.setOnClickListener()
+        {
+            txtCartel.text = textoCartel
+        }
     }
 }
