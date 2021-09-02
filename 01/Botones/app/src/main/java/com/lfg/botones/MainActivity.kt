e14d418349ee2bc07490225a80a6a17ad2b2da48
leonelgomez1990 <leonelgomez@live.com.ar>
2021-09-01 23:14:50 -0300

    Agrego tres botones para cambiar de color el texto, y un 4to boton para borrarlo. Hay un text que indica a modo debug la accion realizada

--- 01/Botones/app/src/main/java/com/lfg/botones/MainActivity.kt
+++ 01/Botones/app/src/main/java/com/lfg/botones/MainActivity.kt
@@ -2,10 +2,58 @@ package com.lfg.botones
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnRed : Button
+    lateinit var btnBlue : Button
+    lateinit var btnYellow: Button
+    lateinit var btnDelete: Button
+    lateinit var txtCartel : TextView
+    lateinit var txtMensaje : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Binding
+        btnRed = findViewById(R.id.btnRed)
+        btnBlue = findViewById(R.id.btnBlue)
+        btnYellow = findViewById(R.id.btnYellow)
+        btnDelete = findViewById(R.id.btnDelete)
+        txtCartel = findViewById(R.id.txtCartel)
+        txtMensaje  = findViewById(R.id.txtMensaje)
+    }
+
+    private fun printColor(color : Int) : String {
+        return getString(R.string.msg_color).plus(" ".plus(getString(color)))
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        btnRed.setOnClickListener(){
+            txtCartel.text = getString(R.string.color_Red)
+            txtCartel.setTextColor(ContextCompat.getColor(this,R.color.red))
+            txtMensaje.text = printColor(R.string.color_Red)
+        }
+        btnYellow.setOnClickListener(){
+            txtCartel.text = getString(R.string.color_Yellow)
+            txtCartel.setTextColor(ContextCompat.getColor(this,R.color.yellow))
+            txtMensaje.text = printColor(R.string.color_Yellow)
+        }
+        btnBlue.setOnClickListener(){
+            txtCartel.text = getString(R.string.color_Blue)
+            txtCartel.setTextColor(ContextCompat.getColor(this,R.color.blue))
+            txtMensaje.text = printColor(R.string.color_Blue)
+        }
+        btnDelete.setOnClickListener(){
+            txtCartel.text = ""
+            txtCartel.setTextColor(ContextCompat.getColor(this,R.color.black))
+            txtMensaje.text = getString(R.string.msg_delete)
+        }
     }
 }
