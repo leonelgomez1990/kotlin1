57036bc741ccfb80ad54e5f4f4c5edc62b574243
leonelgomez1990 <leonelgomez@live.com.ar>
2021-09-04 00:01:06 -0300

    creo clase Mascota

---
+++ 02/kotlinclasses/app/src/main/java/com/lfg/kotlinclasses/entities/Mascota.kt
@@ -0,0 +1,32 @@
+package com.lfg.kotlinclasses.entities
+
+class Mascota (nombre : String, tipo : String, raza : String, edad : Int) {
+    var nombre : String
+    var tipo : String
+    var raza : String
+    var edad : Int
+
+    init {
+        this.nombre = nombre
+        this.tipo   = tipo
+        this.raza   = raza
+        this.edad   = edad
+    }
+
+    fun CalcularEdad (edad : Int) : Int {
+        var edadnueva : Int
+        edadnueva = edad*7
+        return edadnueva
+    }
+
+    class Constants {
+        companion object {
+            val typeGato = "GATO"
+            val typePerro = "PERRO"
+        }
+    }
+
+    override fun toString(): String {
+        return "Mascota(nombre='$nombre', tipo='$tipo', raza='$raza', edad=$edad)"
+    }
+}
