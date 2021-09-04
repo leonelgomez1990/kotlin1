26d9d618a4fb00b05d259b0f6fd26e632fcf0c82
leonelgomez1990 <leonelgomez@live.com.ar>
2021-09-04 14:49:34 -0300

    navegacion 2 fragments

--- 02/NavigationTest/app/src/main/java/com/lfg/navigationtest/fragments/Fragment1.kt
+++ 02/NavigationTest/app/src/main/java/com/lfg/navigationtest/fragments/Fragment1.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import com.lfg.navigationtest.R
 import com.lfg.navigationtest.viewmodels.Fragment1ViewModel
 
@@ -16,12 +18,28 @@ class Fragment1 : Fragment() {
     }
 
     private lateinit var viewModel: Fragment1ViewModel
+    lateinit var v : View
+    lateinit var btnNavegar : Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment1_fragment, container, false)
+        v = inflater.inflate(R.layout.fragment1_fragment, container, false)
+        btnNavegar = v.findViewById(R.id.btnNavigate)
+        return v
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        btnNavegar.setOnClickListener {
+            navegar()
+        }
+    }
+    fun navegar (){
+        var action = Fragment1Directions.actionFragment1ToFragment2()
+        findNavController().navigate(action)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
