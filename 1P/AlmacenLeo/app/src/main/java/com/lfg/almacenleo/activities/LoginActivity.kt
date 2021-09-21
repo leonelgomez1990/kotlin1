21372a0eaed233822b9d2880a1172f2c090468a6
leonelgomez1990 <leonelgomez@live.com.ar>
2021-09-21 16:50:34 -0300

    agrego activities y navgraph

--- 1P/AlmacenLeo/app/src/main/java/com/lfg/almacenleo/activities/LoginActivity.kt
+++ 1P/AlmacenLeo/app/src/main/java/com/lfg/almacenleo/activities/LoginActivity.kt
@@ -1,11 +1,12 @@
-package com.lfg.almacenleo
+package com.lfg.almacenleo.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lfg.almacenleo.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
