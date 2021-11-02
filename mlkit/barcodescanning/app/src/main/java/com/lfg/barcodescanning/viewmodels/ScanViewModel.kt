e1fecffa9cb176c6a7f0ee6914bc25392cb00eb0
leonelgomez1990 <leonelgomez@live.com.ar>
2021-11-02 02:26:03 -0300

    barcode scan working

--- mlkit/barcodescanning/app/src/main/java/com/lfg/barcodescanning/viewmodels/ScanViewModel.kt
+++ mlkit/barcodescanning/app/src/main/java/com/lfg/barcodescanning/viewmodels/ScanViewModel.kt
@@ -1,7 +1,11 @@
 package com.lfg.barcodescanning.viewmodels
 
 import androidx.lifecycle.ViewModel
+import com.lfg.barcodescanning.clases.CameraHelper
 
 class ScanViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    lateinit var cameraHelper: CameraHelper
+
+
 }
+
