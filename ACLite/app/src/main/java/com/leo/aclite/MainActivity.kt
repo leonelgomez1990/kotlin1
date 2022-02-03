258d8d8166229cf534a7a1665fdfd49a3005f995
leonelgomez1990 <leonelgomez@live.com.ar>
2022-02-03 14:20:22 -0300

    simple login with progress, click with scope

--- ACLite/app/src/main/java/com/leo/aclite/MainActivity.kt
+++ ACLite/app/src/main/java/com/leo/aclite/MainActivity.kt
@@ -2,10 +2,44 @@ package com.leo.aclite
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
+import com.leo.aclite.databinding.ActivityMainBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.progress.visibility = View.GONE
+
+        binding.button.setOnClickListener {
+            lifecycleScope.launch {
+                binding.button.visibility = View.GONE
+                binding.progress.visibility = View.VISIBLE
+
+                tryLogin(binding.user.text.toString(), binding.pass.text.toString() )
+
+                binding.button.visibility = View.VISIBLE
+                binding.progress.visibility = View.GONE
+            }
+
+        }
+    }
+
+    private suspend fun tryLogin(user: String, pass: String) {
+        delay(2000)
+        Toast.makeText(
+            this,
+            "user: $user, pass: $pass",
+            Toast.LENGTH_SHORT
+        ).show()
     }
 }
