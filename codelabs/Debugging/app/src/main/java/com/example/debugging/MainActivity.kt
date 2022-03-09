456c01a98bcaee223fe84537bb8fa7844d8366ba
leonelgomez1990 <leonelgomez@live.com.ar>
2022-03-09 10:11:09 -0300

    codelab debugging app: test errors

--- codelabs/Debugging/app/src/main/java/com/example/debugging/MainActivity.kt
+++ codelabs/Debugging/app/src/main/java/com/example/debugging/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.debugging
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 
 private const val TAG = "MainActivity"
 
@@ -10,7 +11,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val helloTextView: TextView = findViewById(R.id.division_textview)
+        helloTextView.text = "Hello, debugging!"
         logging()
+        division()
     }
 
     fun logging() {
@@ -19,5 +23,16 @@ class MainActivity : AppCompatActivity() {
         Log.i(TAG, "INFO: reporting technical information, such as an operation succeeding")
         Log.d(TAG, "DEBUG: reporting technical information useful for debugging")
         Log.v(TAG, "VERBOSE: more verbose than DEBUG logs")
+    }
+
+    fun division() {
+        val numerator = 60
+        var denominator = 4
+        repeat(4) {
+            Thread.sleep(3000)
+            Log.v(TAG, "${numerator / denominator}")
+            findViewById<TextView>(R.id.division_textview).setText("${numerator / denominator}")
+            denominator--
+        }
     }
 }
